67176e9690c4314fa5f240aede0a89eb165c6e98
dinesh-m4 <dinesh.m@edvoy.com>
2022-02-21 17:41:06 +0530

    changes

--- admin/src/components/CKEditor/index.js
+++ admin/src/components/CKEditor/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
-import ClassicEditor from "zudo-ckeditor5";
+import ClassicEditor from "ckeditor5-edvoy";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
   .ck-editor__main {
     min-height: 200px;
-    > div { 
+    > div {
       min-height: 200px;
     }
 
@@ -17,11 +17,90 @@ const Wrapper = styled.div`
       line-height: initial;
     }
     /* Revert font styling */
-    div, span, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre, a, abbr, acronym, address, big, cite, code, del, dfn, em, img, ins, kbd, q, s, samp, small, strike, strong, sub, sup, tt, var, b, u, i, center, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, embed, figure, figcaption, footer, header, hgroup, menu, nav, output, ruby, section, summary, time, mark, audio, video {
+    div,
+    span,
+    applet,
+    object,
+    iframe,
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6,
+    p,
+    blockquote,
+    pre,
+    a,
+    abbr,
+    acronym,
+    address,
+    big,
+    cite,
+    code,
+    del,
+    dfn,
+    em,
+    img,
+    ins,
+    kbd,
+    q,
+    s,
+    samp,
+    small,
+    strike,
+    strong,
+    sub,
+    sup,
+    tt,
+    var,
+    b,
+    u,
+    i,
+    center,
+    dl,
+    dt,
+    dd,
+    ol,
+    ul,
+    li,
+    fieldset,
+    form,
+    label,
+    legend,
+    table,
+    caption,
+    tbody,
+    tfoot,
+    thead,
+    tr,
+    th,
+    td,
+    article,
+    aside,
+    canvas,
+    details,
+    embed,
+    figure,
+    figcaption,
+    footer,
+    header,
+    hgroup,
+    menu,
+    nav,
+    output,
+    ruby,
+    section,
+    summary,
+    time,
+    mark,
+    audio,
+    video {
       font: revert;
     }
     /* Revert list styling */
-    ul, ol {
+    ul,
+    ol {
       list-style: initial;
       margin-left: 16px;
     }
